621c541191e3948e0dae37e978974081a1d1fc3c
RamonMauricioDev <ramonmauriciodev@gmail.com>
2023-11-29 20:59:57 -0300

    Added Header.

---
+++ src/app/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './header.css';
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <h6 className="header-button">Arsenal</h6>
+            <h6 className="header-button">Home</h6>
+            <h6 className="header-button">Contact</h6>
+            <h6 className="header-button">Projects</h6>
+        </div>
+    )
+}
+
+export default Header
