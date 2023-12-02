481867051b9e2b2af3df1952b26c81b43fe0e62e
RamonMauricioDev <ramonmauriciodev@gmail.com>
2023-12-02 10:30:23 -0300

    Arsenal added, still need to be responsive.

--- src/app/components/arsenal/arsenal.jsx
+++ src/app/components/arsenal/arsenal.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import Image from 'next/image'
 import './arsenal.css';
+
+const IconList = ["/css.png", "/figma.png", "/firebase.png", "/github.png", "/html-5.png", "/jest.png", "/js.png", 
+"/next-js.png", "/reactjs.png", "/webpack.png"]
+
+const ArsenalList = ["The Basic: HTML, CSS.", "Programming Languages: Javascript.", 
+"Frameworks: React JS", "Tools: Figma, Firebase, Git, Webpack."]
 
 const Arsenal = () =>{
     return(
-        <div>
-            arsenal
+        <div className="arsenal">
+            <h2>Arsenal</h2>
+            <div className="arsenal-flex">
+                <p>
+                    {ArsenalList.map((arsenal) => 
+                    <div key={arsenal.toString()}>
+                        <span className="smallbr" />
+                        {arsenal}
+                    </div>)}
+                </p>
+                <div className="icon-grid">
+                {IconList.map((icon) =>
+                    <Image src={icon} key={icon.toString()} width={32} height={32}/>)}
+                </div>
+            </div>
         </div>
     )
 }
