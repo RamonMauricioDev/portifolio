c1eb90da8d686f12cc12d39b2a094a4f49a99b8c
RamonMauricioDev <ramonmauriciodev@gmail.com>
2023-12-06 16:45:47 -0300

    Structure and responsiviness project section done.

--- src/app/components/projects/projects.jsx
+++ src/app/components/projects/projects.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+//import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Image from 'next/image'
 import './projects.css';
+import { db } from "@/config/firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 const Projects = () =>{
+
+    const projectStyle = {
+        width: "clamp(300px, 34vw, 600px)",
+        height: "clamp(150px, 17vw, 300px)"
+    }
+
+    const projectDb = collection(db, "portifolio");
+    const [projectList, setProjectList] = useState([]);
+    useEffect(() => {
+        const getProjectList = async () => {
+            try {
+                const data = await getDocs(projectDb)
+                const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
+                setProjectList(filteredData);
+            } catch(err) {
+                console.error(err);
+            }
+        };
+        getProjectList();
+    }, [])
+
+    console.log(projectList)
+
     return(
         <div>
-            Projects
+            <h2 className="project-title">Personal Projects</h2>
+            <div className="want-to-see-more">
+                <h5>Here are a few of my personal projects. Want to see more ?</h5>
+                <h5 className="primary-color">Email me.</h5>
+            </div>
+            <div className="project-flex">
+            {projectList.map((project) =>
+                <div style={{borderRadius: '15px', overflow: 'hidden'}}>
+                    <a href={project.GithubProject} target="_blank">
+                    <Image loader={() => project.Image} src = {project.Image}  key = {project.Image.toString()} width={600} height={300} quality={100} style={projectStyle}/>
+                    </a>
+                </div>
+            )}
+            </div>
         </div>
     )
 }
