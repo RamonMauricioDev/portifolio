6903a3482c5be2ac75338597fb21859ef8c4c22f
RamonMauricioDev <ramonmauriciodev@gmail.com>
2023-12-13 17:51:19 -0300

    added href to all h6.

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -4,10 +4,10 @@ import './header.css';
 const Header = () =>{
     return(
         <div className="header">
-            <h6 className="header-button">Home</h6>
-            <h6 className="header-button">Arsenal</h6>
-            <h6 className="header-button">Projects</h6>
-            <h6 className="header-button">Contact</h6>
+            <h6><a href="#home" className="header-button">Home</a></h6>
+            <h6><a href="#arsenal" className="header-button">Arsenal</a></h6>
+            <h6><a href="#projects" className="header-button">Projects</a></h6>
+            <h6><a href="#contact" className="header-button">Contact</a></h6>
         </div>
     )
 }
